2b6cbc80d40c9dac6a65028dcd0045b316483525
basedwell <maxwell.d.masters@live.com>
2025-08-14 14:53:33 -0400

    Update type_constants.asm

--- constants/type_constants.asm
+++ constants/type_constants.asm
@@ -15,8 +15,9 @@ DEF PHYSICAL EQU const_value
 	const ROCK
 	const BIRD
 	const BUG
-	const GHOST
+	const DARK
 	const STEEL
+	const DRAGON
 
 DEF UNUSED_TYPES EQU const_value
 	const_next 19
@@ -30,8 +31,9 @@ DEF SPECIAL EQU const_value
 	const ELECTRIC
 	const PSYCHIC_TYPE
 	const ICE
-	const DRAGON
-	const DARK
+	const GHOST
+	
+
 DEF TYPES_END EQU const_value
 
 DEF NUM_TYPES EQU TYPES_END + UNUSED_TYPES - UNUSED_TYPES_END - 1 ; discount BIRD
