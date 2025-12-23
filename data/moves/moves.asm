75888e9a15764eb5a7a40e50dfc26301bc32357a
basedwell <maxwell.d.masters@live.com>
2025-12-22 19:51:53 -0500

    Update moves.asm

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -266,5 +266,5 @@ Moves:
 	move WHIRLPOOL,    EFFECT_TRAP_TARGET,        15, WATER,         70, 15,   0
 	move BEAT_UP,      EFFECT_BEAT_UP,            10, DARK,         100, 10,   0
 	move BRICK_BREAK,  EFFECT_NORMAL_HIT,         75, FIGHT,        100, 15,   0
-	move BULLET_PUNCH, EFFECT_PRIORITY_HIT        40, STEEL,        100, 20,   0
+	move BULLET_PUNCH, EFFECT_PRIORITY_HIT,       40, STEEL,        100, 20,   0
 	assert_table_length NUM_ATTACKS
