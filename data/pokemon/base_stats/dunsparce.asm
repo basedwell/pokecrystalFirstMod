0eb80ba186c0ea5640e0314e12d6ac1ee6295ac6
basedwell <maxwell.d.masters@live.com>
2026-01-31 20:13:39 -0500

    Update dunsparce.asm

--- data/pokemon/base_stats/dunsparce.asm
+++ data/pokemon/base_stats/dunsparce.asm
@@ -1,11 +1,11 @@
 	db DUNSPARCE ; 206
 
-	db 100,  70,  70,  45,  65,  65
+	db 125, 100,  70,  50,  85,  70
 	;   hp  atk  def  spd  sat  sdf
 
 	db NORMAL, NORMAL ; type
 	db 190 ; catch rate
-	db 75 ; base exp
+	db 159 ; base exp
 	db NO_ITEM, NO_ITEM ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
